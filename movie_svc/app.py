6994bf36dc617529d216301ed84736a5a2dca50d
dastagg(opus) <gitconfig>
2020-02-09 10:47:44 -0500

    All three routes are working with data

--- movie_svc/app.py
+++ movie_svc/app.py
@@ -1,32 +1,48 @@
 import responder
 
+from data import db
+
 api = responder.API()
+db.global_init()
+
+response_count_max = 10
 
 
 @api.route("/")
-def index(req, resp):
+def index(_, resp):
     resp.content = api.template("home/index.html")
 
 
 # Search movies
 # GET /api/search/{keyword}
 @api.route("/api/search/{keyword}")
-def search_by_keyword(req, resp: responder.Response, keyword: str):
-    resp.media = {"searching": keyword}
+def search_by_keyword(_, resp: responder.Response, keyword: str):
+    movies = db.search_keyword(keyword)
+    limited = len(movies) > response_count_max
+    if limited:
+        movies = movies[:10]
+    movie_dicts = [db.movie_to_dict(m) for m in movies]
+    resp.media = {"keyword": keyword, "hits": movie_dicts, "truncated_results": limited}
 
 
 # Movies by director
 # GET /api/director/{director_name}
 @api.route("/api/director/{director_name}")
-def search_by_director(req, resp: responder.Response, director_name: str):
-    resp.media = {"searching": director_name}
+def search_by_director(_, resp: responder.Response, director_name: str):
+    movies = db.search_director(director_name)
+    limited = len(movies) > response_count_max
+    if limited:
+        movies = movies[:response_count_max]
+    movie_dicts = [db.movie_to_dict(m) for m in movies]
+    resp.media = {"keyword": director_name, "hits": movie_dicts, "truncated_results": limited}
 
 
 # Movie by IMDB code
 # GET /api/movie/{imdb_number}
 @api.route("/api/movie/{imdb_number}")
-def movie_by_imdb(req, resp: responder.Response, imdb_number: str):
-    resp.media = {"searching": imdb_number}
+def movie_by_imdb(_, resp: responder.Response, imdb_number: str):
+    movie = db.find_by_imdb(imdb_number)
+    resp.media = db.movie_to_dict(movie)
 
 
 api.run()
